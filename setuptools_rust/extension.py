baa78ad05b07eefcaa85c44c1e4680b1431fc7f4
Nikolay Kim <fafhrd91@gmail.com>
2017-09-08 11:35:51 -0700

    fix script generation for executables

--- setuptools_rust/extension.py
+++ setuptools_rust/extension.py
@@ -123,10 +123,10 @@ import sys
 
 def run():
     path = os.path.split(__file__)[0]
-    file = os.path.join(path, "%(name)")
+    name = os.path.split(sys.argv[0])[1]
+    file = os.path.join(path, name)
     if os.path.isfile(file):
         os.execv(file, sys.argv)
     else:
-        print("Can not execute %(name)")
-
+        print("Can not execute '%s'" % name)
 """
