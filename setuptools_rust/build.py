c339c22137b4ff8c9db9a031bc5635b425b20735
Mario Prince <mario.prince@outlook.com>
2017-12-03 18:36:20 -0500

    ignorer l'option strip pour win32

--- setuptools_rust/build.py
+++ setuptools_rust/build.py
@@ -218,20 +218,21 @@ class build_rust(Command):
 
             shutil.copyfile(dylib_path, ext_path)
 
-            if not debug_build:
-                args = []
-                if ext.strip == Strip.All:
-                    args.append('-x')
-                elif ext.strip == Strip.Debug:
-                    args.append('-S')
+            if sys.platform != "win32":
+                if not debug_build:
+                    args = []
+                    if ext.strip == Strip.All:
+                        args.append('-x')
+                    elif ext.strip == Strip.Debug:
+                        args.append('-S')
 
-                if args:
-                    args.insert(0, 'strip')
-                    args.append(ext_path)
-                    try:
-                        output = subprocess.check_output(args, env=env)
-                    except subprocess.CalledProcessError as e:
-                        pass
+                    if args:
+                        args.insert(0, 'strip')
+                        args.append(ext_path)
+                        try:
+                            output = subprocess.check_output(args, env=env)
+                        except subprocess.CalledProcessError as e:
+                            pass
 
             if executable:
                 mode = os.stat(ext_path).st_mode
