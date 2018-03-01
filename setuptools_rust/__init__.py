795120034f3218f9b4aed3ecc2a2922adf5463ae
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-03-01 23:11:21 +0100

    Add `tomlgen_rust` to distutils commands

--- setuptools_rust/__init__.py
+++ setuptools_rust/__init__.py
@@ -5,8 +5,9 @@ from .build import build_rust
 from .build_ext import build_ext
 from .check import check_rust
 from .clean import clean_rust
-from .test import test_rust
 from .extension import RustExtension
+from .test import test_rust
+from .tomlgen import tomlgen_rust, find_rust_extensions
 from .utils import Binding, Strip
 
 __all__ = ('RustExtension', 'Binding', 'Strip',
