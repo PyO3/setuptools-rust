238ed830ffdddf5f8e434e6ee683514ab725b6a2
konstin <konstin@mailbox.org>
2018-10-10 19:34:06 +0200

    Try to fix CI

--- example_tomlgen/hello/english/lib.rs
+++ example_tomlgen/hello/english/lib.rs
@@ -1,12 +1,10 @@
-#![feature(use_extern_macros)]
-
 extern crate pyo3;
 
 use pyo3::prelude::*;
 
 /// Module documentation string
 #[pymodinit]
-fn english(py: Python, m: &PyModule) -> PyResult<()> {
+fn english(_py: Python, m: &PyModule) -> PyResult<()> {
 
     #[pyfn(m, "hello")]
     fn hello(_py: Python) -> PyResult<()> {
--- example_tomlgen/hello/french/lib.rs
+++ example_tomlgen/hello/french/lib.rs
@@ -1,12 +1,10 @@
-#![feature(use_extern_macros)]
-
 extern crate pyo3;
 
 use pyo3::prelude::*;
 
 /// Module documentation string
 #[pymodinit]
-fn french(py: Python, m: &PyModule) -> PyResult<()> {
+fn french(_py: Python, m: &PyModule) -> PyResult<()> {
 
     #[pyfn(m, "hello")]
     fn hello(_py: Python) -> PyResult<()> {
