7125e1973a039d93358baf27f70b40d3d8553034
Alex Gaynor <alex.gaynor@gmail.com>
2020-07-27 13:19:29 -0400

    Migrate this to use slightly more constrainted monkeypatching
    The primary benefit of this is that it'll work when you have a single project using both setuptools-rust as well as cffi.

--- setuptools_rust/__init__.py
+++ setuptools_rust/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import print_function, absolute_import
 
-from . import patch
 from .build import build_rust
-from .build_ext import build_ext
 from .check import check_rust
 from .clean import clean_rust
 from .extension import RustExtension
@@ -16,10 +14,6 @@ __all__ = (
     "Strip",
     "check_rust",
     "clean_rust",
-    "build_ext",
     "build_rust",
     "test_rust",
 )
-
-
-patch.monkey_patch_dist(build_ext)
