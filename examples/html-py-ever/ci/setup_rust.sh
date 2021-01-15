9fa6f593d86caabf9e4187e5811335e653b27439
0kalekale <kalekale.anon@gmail.com>
2021-01-15 12:55:49 +0545

    removed .travis ci to switch to github workflows

--- examples/html-py-ever/ci/setup_rust.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-### Setup Rust toolchain #######################################################
-
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
-export PATH=$PATH:$HOME/.cargo/bin
