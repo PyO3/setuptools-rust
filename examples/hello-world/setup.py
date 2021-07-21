b56d126852215a10d075b654631caac356b6a253
messense <messense@icloud.com>
2021-07-21 15:51:12 +0800

    Fix `RustExtension.install_script` (#154)
    * Fix `RustExtension.install_script`
    
    * Add exec binding example
    
    * Make directories for exec binding extension
    
    * Update changelog for #154

---
+++ examples/hello-world/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="hello-world",
+    version="1.0",
+    rust_extensions=[
+        RustExtension("hello_world.hello_world", binding=Binding.Exec, script=True)
+    ],
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+)
