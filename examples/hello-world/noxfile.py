be0a456c014229df0d0471b95db8cd133f9f2edb
messense <messense@icloud.com>
2022-06-17 10:29:41 +0800

    Run `pip install --no-build-isolation` in examples

--- examples/hello-world/noxfile.py
+++ examples/hello-world/noxfile.py
@@ -8,5 +8,5 @@ SETUPTOOLS_RUST = dirname(dirname(dirname(__file__)))
 @nox.session()
 def test(session: nox.Session):
     session.install(SETUPTOOLS_RUST)
-    session.install(".")
+    session.install("--no-build-isolation", ".")
     session.run("hello-world", *session.posargs)
--- examples/rust_with_cffi/noxfile.py
+++ examples/rust_with_cffi/noxfile.py
@@ -8,5 +8,5 @@ SETUPTOOLS_RUST = dirname(dirname(dirname(__file__)))
 @nox.session()
 def test(session: nox.Session):
     session.install(SETUPTOOLS_RUST, "pytest", "cffi")
-    session.install(".")
+    session.install("--no-build-isolation", ".")
     session.run("pytest", *session.posargs)
