239595b96e4aabf538db37d0040338fef3f26d0f
messense <messense@icloud.com>
2022-07-30 13:14:07 +0800

    Simplify `install_rust_extension`

--- setuptools_rust/setuptools_ext.py
+++ setuptools_rust/setuptools_ext.py
@@ -152,19 +152,10 @@ def add_rust_extension(dist: Distribution) -> None:
 
     install_base_class = cast(Type[install], dist.cmdclass.get("install", install))
 
-    # this is required because, install directly access distribution's
-    # ext_modules attr to check if dist has ext modules
+    # this is required for console_scripts and to make install_scripts compatible with RustBin
     class install_rust_extension(install_base_class):  # type: ignore[misc,valid-type]
         def finalize_options(self) -> None:
-            ext_modules = self.distribution.ext_modules
-
-            # all ext modules
-            mods = []
-            if self.distribution.ext_modules:
-                mods.extend(self.distribution.ext_modules)
             if self.distribution.rust_extensions:
-                mods.extend(self.distribution.rust_extensions)
-
                 scripts = []
                 for ext in self.distribution.rust_extensions:
                     scripts.extend(ext.entry_points())
@@ -185,20 +176,16 @@ def add_rust_extension(dist: Distribution) -> None:
 
                         self.distribution.entry_points["console_scripts"] = ep_scripts
 
-            self.distribution.ext_modules = mods
-
             install_base_class.finalize_options(self)
-
-            # restore ext_modules
-            self.distribution.ext_modules = ext_modules
 
         def run(self) -> None:
             install_base_class.run(self)
             install_rustbin = False
             if self.distribution.rust_extensions:
-                for ext in self.distribution.rust_extensions:
-                    if isinstance(ext, RustBin):
-                        install_rustbin = True
+                install_rustbin = any(
+                    isinstance(ext, RustBin)
+                    for ext in self.distribution.rust_extensions
+                )
             if install_rustbin:
                 self.run_command("install_scripts")
 
