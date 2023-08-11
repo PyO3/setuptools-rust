8530b868275d9849dc19233450b183205e3bc14f
Anderson Bravalheri <andersonbravalheri@gmail.com>
2023-08-11 12:11:32 +0100

    Use pyproject.toml for hello-world-script example

--- examples/hello-world-script/noxfile.py
+++ examples/hello-world-script/noxfile.py
@@ -8,12 +8,9 @@ SETUPTOOLS_RUST = dirname(dirname(dirname(__file__)))
 @nox.session()
 def test(session: nox.Session):
     session.install(SETUPTOOLS_RUST, "wheel")
+    # Ensure build uses version of setuptools-rust under development
     session.install("--no-build-isolation", ".")
+    # Test Rust binary
     session.run("hello-world-script", *session.posargs)
-
-
-@nox.session()
-def setuptools_install(session: nox.Session):
-    session.install(SETUPTOOLS_RUST)
-    session.run("python", "setup.py", "install")
-    session.run("hello-world-script", *session.posargs)
+    # Test Python package
+    session.run("python", "-c", "import hello_world; print(hello_world)")
