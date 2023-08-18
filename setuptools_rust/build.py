a808242dbaee620ad4235b43f9a8cb60bf81e005
Anderson Bravalheri <andersonbravalheri@gmail.com>
2023-08-18 08:41:29 +0100

    Fix linting problem

--- setuptools_rust/build.py
+++ setuptools_rust/build.py
@@ -780,8 +780,7 @@ def _replace_cross_target_dir(path: str, ext: RustExtension, *, quiet: bool) -> 
 
 
 def _get_bdist_wheel_cmd(  # type: ignore[no-any-unimported]
-    dist: Distribution,
-    create: bool = True
+    dist: Distribution, create: bool = True
 ) -> Optional[CommandBdistWheel]:
     try:
         cmd_obj = dist.get_command_obj("bdist_wheel", create=create)
