6d94c40d1ff5610d6f52c3e58de4dab77799a061
Anderson Bravalheri <andersonbravalheri@gmail.com>
2023-08-20 14:43:47 +0100

    Modify setup.py example to be easier to write a tutorial

---
+++ examples/hello-world-setuppy/python/hello_world/__init__.py
@@ -0,0 +1,7 @@
+from ._lib import sum_as_string
+
+__all__ = ["sum_as_string"]
+
+# It is a common practice in Python packaging to keep the extension modules
+# private and use Pure Python modules to wrap them.
+# This allows you to have a very fine control over the public API.
