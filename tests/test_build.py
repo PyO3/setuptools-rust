55866aa5fe8f5fc85f3198608428aeb104e57b9d
Jake Lishman <jake@binhbar.com>
2026-02-21 18:24:59 +0000

    Isolate `universal2` artifact-combination logic (#572)

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,16 +1,21 @@
 from unittest import mock
 
-from setuptools_rust.build import _adjusted_local_rust_target
+from setuptools_rust.build import _override_cargo_default_target
+from setuptools_rust._utils import Env
 
 
-def test_adjusted_local_rust_target_windows_msvc():
+NO_ENV = Env(None)
+
+
+def test_override_cargo_default_target_windows_msvc():
     with mock.patch(
         "setuptools_rust.rustc_info.get_rust_target_info",
         lambda _plat_name, _env: ["target_env=msvc"],
     ):
-        assert _adjusted_local_rust_target("win32", None) == "i686-pc-windows-msvc"
+        assert _override_cargo_default_target("win32", NO_ENV) == "i686-pc-windows-msvc"
         assert (
-            _adjusted_local_rust_target("win-amd64", None) == "x86_64-pc-windows-msvc"
+            _override_cargo_default_target("win-amd64", NO_ENV)
+            == "x86_64-pc-windows-msvc"
         )
 
 
@@ -19,10 +24,15 @@ def test_adjusted_local_rust_target_windows_gnu():
         "setuptools_rust.rustc_info.get_rust_target_info",
         lambda _plat_name, _env: ["target_env=gnu"],
     ):
-        assert _adjusted_local_rust_target("win32", None) == "i686-pc-windows-gnu"
-        assert _adjusted_local_rust_target("win-amd64", None) == "x86_64-pc-windows-gnu"
+        assert _override_cargo_default_target("win32", NO_ENV) == "i686-pc-windows-gnu"
+        assert (
+            _override_cargo_default_target("win-amd64", NO_ENV)
+            == "x86_64-pc-windows-gnu"
+        )
 
 
 def test_adjusted_local_rust_target_macos():
     with mock.patch("platform.machine", lambda: "x86_64"):
-        assert _adjusted_local_rust_target("macosx-", None) == "x86_64-apple-darwin"
+        assert (
+            _override_cargo_default_target("macosx-", NO_ENV) == "x86_64-apple-darwin"
+        )
